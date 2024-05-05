e4117c8d3538ccede5970cbd7ba3b9c95da203c9
Dagadev <gacub20ru@gmail.com>
2024-05-05 13:32:34 -0600

    Add ingredient and product interface

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,19 @@
 export interface Recipe {
   id: string
   name: string
-  ingredients: Array<{
-    id: string
-    name: string
-    price: number
-    quantity: number
-    unitOfMeasurement: 'g' | 'k' | 'ml' | 'l'
-  }>
+  ingredients: Ingredient[]
+}
+
+export interface Ingredient {
+  unit: 'g' | 'k' | 'ml' | 'l'
+  productId: string
+  quantity: number
+}
+
+export interface Product {
+  id: string
+  name: string
+  unit: 'g' | 'k' | 'ml' | 'l'
+  price: number
+  quantity: number
 }
