568da481538029dfeb74c81aaf1afa61d3e07843
Dagadev <gacub20ru@gmail.com>
2024-05-07 19:20:58 -0600

    Add persist middleware

--- src/store/products-store.ts
+++ src/store/products-store.ts
@@ -1,25 +1,31 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 import type { Product } from '@/types'
 
 interface ProductsStore {
   products: Product[]
+  addProduct: (newProduct: Omit<Product, 'id'>) => void
+  editProduct: (productId: string, product: Omit<Product, 'id'>) => void
+  removeProduct: (productId: string) => void
 }
 
-export const useProductsStore = create<ProductsStore>()(set => ({
-  products: [
-    {
-      id: '2l3k',
-      name: 'harina de trigo',
-      price: 18,
-      quantity: 1,
-      unit: 'k'
-    },
-    {
-      id: '324-l5',
-      name: 'sal',
-      price: 10,
-      quantity: 500,
-      unit: 'g'
-    }
-  ]
+export const useProductsStore = create<ProductsStore>()(persist(set => ({
+  products: [],
+  addProduct (newProduct) {
+    set(({ products }) => ({
+      products: [...products, { id: crypto.randomUUID(), ...newProduct }]
+    }))
+  },
+  editProduct (productId, product) {
+    set(({ products }) => ({
+      products: products.map(p => p.id === productId ? { ...p, ...product } : p)
+    }))
+  },
+  removeProduct (productId) {
+    set(({ products }) => ({
+      products: products.filter(p => p.id !== productId)
+    }))
+  }
+}), {
+  name: 'products'
 }))
--- src/store/recipes-store.ts
+++ src/store/recipes-store.ts
@@ -1,37 +1,49 @@
 import { create } from 'zustand'
-import type { Recipe } from '../types'
-
-const defaultRecipes: Recipe[] = [
-  {
-    id: '123k',
-    name: 'pizza',
-    ingredients: [
-      {
-        id: '2l3k',
-        name: 'harina de trigo',
-        price: 18,
-        quantity: 1,
-        unitOfMeasurement: 'k'
-      },
-      {
-        id: '324-l5',
-        name: 'sal',
-        price: 10,
-        quantity: 500,
-        unitOfMeasurement: 'g'
-      }
-    ]
-  }
-]
+import { persist } from 'zustand/middleware'
+import type { Recipe, Ingredient } from '../types'
 
 interface RecipesStore {
   recipes: Recipe[]
-  addRecipe: () => void
+  addRecipe: (newRecipe: Omit<Recipe, 'id'>) => void
+  editRecipe: (recipeId: string, recipe: Omit<Recipe, 'id' | 'ingredients'>) => void
+  removeRecipe: (recipeId: string) => void
+  addIngredient: (recipeId: string, newIngredient: Ingredient) => void
+  editIngredient: (recipeId: string, productId: string, ingredient: Ingredient) => void
+  removeIngredient: (recipeId: string, productId: string) => void
 }
 
-export const useRecipesStore = create<RecipesStore>()(set => ({
-  recipes: defaultRecipes,
-  addRecipe () {
-
+export const useRecipesStore = create<RecipesStore>()(persist(set => ({
+  recipes: [],
+  addRecipe (newRecipe) {
+    set(({ recipes }) => ({
+      recipes: [...recipes, { id: crypto.randomUUID(), ...newRecipe }]
+    }))
+  },
+  editRecipe (recipeId, recipe) {
+    set(({ recipes }) => ({
+      recipes: recipes.map(r => r.id === recipeId ? { ...r, ...recipe } : r)
+    }))
+  },
+  removeRecipe (recipeId) {
+    set(({ recipes }) => ({
+      recipes: recipes.filter(r => r.id !== recipeId)
+    }))
+  },
+  addIngredient (recipeId, newIngredient) {
+    set(({ recipes }) => ({
+      recipes: recipes.map(r => r.id === recipeId ? { ...r, ingredients: [...r.ingredients, newIngredient] } : r)
+    }))
+  },
+  editIngredient (recipeId, productId, ingredient) {
+    set(({ recipes }) => ({
+      recipes: recipes.map(r => r.id === recipeId ? { ...r, ingredients: r.ingredients.map(i => i.productId === productId ? ingredient : i) } : r)
+    }))
+  },
+  removeIngredient (recipeId, productId) {
+    set(({ recipes }) => ({
+      recipes: recipes.map(r => r.id === recipeId ? { ...r, ingredients: r.ingredients.filter(i => i.productId !== productId) } : r)
+    }))
   }
+}), {
+  name: 'recipes'
 }))
