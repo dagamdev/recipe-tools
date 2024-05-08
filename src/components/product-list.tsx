da2000f369cf32d42821ee822ccf5f02fcbba211
Dagadev <gacub20ru@gmail.com>
2024-05-07 19:29:19 -0600

    Add edit an remove product functions

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -2,15 +2,47 @@
 
 import { useProductsStore } from '@/store/products-store'
 import ProductsTable from './products-table'
+import CreationButton from './creation-button'
+import { useToolsStore } from '@/store/tools-store'
 
 export default function ProductList () {
-  const products = useProductsStore(store => store.products)
+  const { products, addProduct, editProduct, removeProduct } = useProductsStore()
+  const setTools = useToolsStore(store => store.setTools)
 
   return (
-    <section>
-      <h3 className='text-muted-foreground text-xl font-bold mb-2'>Products:</h3>
+    <section className='space-y-4'>
+      <div className='flex justify-between'>
+        <h3 className='text-2xl font-bold'>Products:</h3>
+        <CreationButton onClick={() => {
+          setTools({
+            type: 'product',
+            action: 'create',
+            manageValues (values) {
+              if ('unit' in values && 'name' in values) {
+                addProduct({ ...values })
+              }
+            },
+            formData: undefined
+          })
+        }} elementType='product' />
+      </div>
 
-      <ProductsTable products={products} caption='A list of reference products for recipe ingredients.' />
+      <ProductsTable products={products}
+        editProduct={product => {
+          setTools({
+            type: 'product',
+            action: 'update',
+            manageValues (values) {
+              if ('unit' in values && 'name' in values) {
+                editProduct(product.id, values)
+              }
+            },
+            formData: product
+          })
+        }}
+        removeProduct={id => { removeProduct(id) }}
+        caption='A list of reference products for recipe ingredients.'
+      />
     </section>
   )
 }
