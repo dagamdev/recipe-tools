5990bc2d1b5bfc10f95dbff4b6ffc55b3fb4fdd5
Dagadev <gacub20ru@gmail.com>
2024-05-07 19:21:32 -0600

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ProductList from '@/components/product-list'
+import RecipeList from '@/components/recipe-list'
+
 export default function Home () {
   return (
-    <main className="p-5">
-      <h1>Recipe tools</h1>
-    </main>
+    <>
+      <RecipeList />
+      <ProductList />
+    </>
   )
 }
