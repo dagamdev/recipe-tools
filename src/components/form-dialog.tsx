da2d5ac9a724f5f0ae0a6a18045934b3f9001287
Dagadev <gacub20ru@gmail.com>
2024-05-11 17:19:31 -0600

    Change dialog-drawer component

---
+++ src/components/form-dialog.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import * as React from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle
+} from '@/components/ui/sheet'
+import { useToolsStore } from '@/store/tools-store'
+import DinamicForm from './dinamic-form'
+
+export default function FormDialog () {
+  const [open, setOpen, action, type, formData] = useToolsStore(store => [store.open, store.setOpen, store.action, store.type, store.formData])
+  const [isDesktop, setIsDesktop] = React.useState(false)
+
+  React.useEffect(() => {
+    if (typeof window === 'undefined') return
+
+    const handleWidht = () => {
+      if (window.innerWidth >= 720) setIsDesktop(true)
+      if (window.innerWidth < 720) setIsDesktop(false)
+    }
+
+    if (typeof window === 'undefined') return
+
+    window.addEventListener('resize', handleWidht)
+
+    return () => {
+      window.removeEventListener('resize', handleWidht)
+    }
+  }, [])
+
+  const description = action === 'create'
+    ? type === 'product'
+      ? 'Create a new product to reference an ingredient in a recipe'
+      : 'Once it is created you can add the ingredients'
+    : `Make changes to your ${type} here. Click save when you're done.`
+
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>{action === 'create'
+              ? `Create ${type}`
+              : `Edit ${type}`}</DialogTitle>
+            <DialogDescription>
+              {description}
+            </DialogDescription>
+          </DialogHeader>
+          <DinamicForm type={type} defaultValues={formData} />
+        </DialogContent>
+      </Dialog>
+    )
+  }
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetContent side={'top'}>
+        <SheetHeader>
+          <SheetTitle>{action === 'create'
+            ? `Create ${type}`
+            : `Edit ${type}`}</SheetTitle>
+          <SheetDescription>
+            {description}
+          </SheetDescription>
+        </SheetHeader>
+        <DinamicForm type={type} defaultValues={formData} />
+      </SheetContent>
+    </Sheet>
+  )
+}
