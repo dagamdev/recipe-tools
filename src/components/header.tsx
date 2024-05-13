0dd7ed8d05f271dd1c53225076e0d0e82f4be301
Dagadev <gacub20ru@gmail.com>
2024-05-13 16:42:34 -0600

    Update style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,13 @@
 import Link from 'next/link'
 import { ModeToggle } from './mode-toggle'
 import HomeIcon from '@/icons/home'
-import { buttonVariants } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
 
 export default function Header () {
   return (
     <header className="flex items-center justify-between h-16 px-4 max-w-4xl mx-auto">
-      <Link className={cn('text-lg font-bold', buttonVariants({ variant: 'ghost' }))} href="/">
+      <Link className='inline-flex items-center text-lg font-bold text-primary underline-offset-4 hover:underline' href="/">
         <HomeIcon /><span className='sm:inline hidden ml-1'>Home</span>
       </Link>
-
       <ModeToggle />
     </header>
   )
