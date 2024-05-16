43570e9b162c9f2db18b3ce934ecfa8b224ac6d1
Dagadev <gacub20ru@gmail.com>
2024-05-16 11:51:18 -0600

    Update header styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import HomeIcon from '@/icons/home'
 
 export default function Header () {
   return (
-    <header className="flex items-center justify-between h-16 px-4 max-w-4xl mx-auto">
+    <header className="flex items-center justify-between h-16 px-4 max-w-4xl mx-auto sticky top-0 bg-background rounded-lg">
       <Link className='inline-flex items-center text-lg font-bold text-primary underline-offset-4 hover:underline' href="/">
         <HomeIcon /><span className='sm:inline hidden ml-1'>Home</span>
       </Link>
